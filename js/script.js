764aae606afd1fdb4b961fcaa1abd48c33fe0b64
wsuits6 <wsuits6@gmail.com>
2026-02-02 17:13:42 +0000

    created commented Js code

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//===================
+//Movie Hub
+//====================
+
+//Flow
+
+// (1) All variables
+const themeToggle = document.getElementById("")
+
+// (2) A Function that handles searches
+
+// (3) A Function that renders the Movie Card
+
+// (4) A Function that renders the Watch list
+
+// (5) A Function that Toggles the Theme
